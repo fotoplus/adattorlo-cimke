d3cf8dafc1334c976a44dfb6202be7b842584d3c
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2021-12-22 17:03:28 +0100

    Update ipcheck.php

--- e/modules/accesscontrol/ipcheck.php
+++ e/modules/accesscontrol/ipcheck.php
@@ -2,7 +2,7 @@
 print $_SERVER['REMOTE_ADDR'];
 print_r($ip_allow_list);
 
-if(!array_key_exists($ip_allow_list, $_SERVER['REMOTE_ADDR'])) {
+if(!array_key_exists($_SERVER['REMOTE_ADDR'], $ip_allow_list)) {
     die('Hozzáférés megtagadva.');
 }
 ?>
