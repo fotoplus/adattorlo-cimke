9a0b53a37d90a419993d50464e0e2c4131725af4
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2021-12-22 17:11:08 +0100

    -

--- e/modules/accesscontrol/ipcheck.php
+++ e/modules/accesscontrol/ipcheck.php
@@ -1,8 +1,9 @@
 <?php
-print $_SERVER['REMOTE_ADDR'];
-print_r($ip_allow_list);
 
 if(!array_key_exists($_SERVER['REMOTE_ADDR'], $ip_allow_list)) {
     die('Hozzáférés megtagadva.');
+} else {
+    $branch = $ip_allow_list[$_SERVER['REMOTE_ADDR']];
 }
+
 ?>
