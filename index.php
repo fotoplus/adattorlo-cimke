3309143fd1b2873e1a0a73c4f4a972294b457727
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2021-12-22 17:08:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require_once ('e/modules/mysql/mysql.php');
 $pages_dir = './e/pages/';
 $_SERVER['REQUEST_URI_PATH'] = preg_replace('/\?.*/', '', $_SERVER['REQUEST_URI']);
 $segments = array_slice (explode('/', trim($_SERVER['REQUEST_URI_PATH'], '/')), URI_IGNORE);
-$page['name'] = isset( $segments[0] ) ? $segments[0] : 'home';
+$page['name'] = !empty( $segments[0] ) ? $segments[0] : 'home';
 
 ?>
 <!doctype html>
