a5301af2a0eaf9d4495755415690818f0a3aac6a
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2021-12-23 22:11:52 +0100

    -

--- e/modules/accesscontrol/ipcheck.php
+++ e/modules/accesscontrol/ipcheck.php
@@ -1,11 +1,11 @@
 <?php
 
-if(!array_key_exists($_SERVER['REMOTE_ADDR'], $ip_allow_list)) {
+if(!array_key_exists($_SERVER['REMOTE_ADDR'], $ip_allow_list)):
    header('Location: '. REDIRECT_URL);
    exit;
-} else {
+else:
     $branch = $ip_allow_list[$_SERVER['REMOTE_ADDR']];
     $allow = true;
-}
+endif;
 
 ?>
