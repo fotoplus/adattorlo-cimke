d27faa2d61c052da8d12f053794d345562809256
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2021-12-23 10:59:10 +0100

    Update feltoltes.php

--- e/pages/feltoltes.php
+++ e/pages/feltoltes.php
@@ -1,5 +1,28 @@
 <?php require_once ('e/modules/accesscontrol/ipcheck.php');
 
-
-
 ?>
+
+<h1>Új átadás/átvétel rögzítése</h1>
+
+<form name="feltoltes" method="post">
+ 
+ <?php
+ ?>
+  <h2>Átadójegyzék</h2>
+
+  <input name="step" type="hidden" value="jegyzek" >
+
+  <legend for="iktatoszam">Átadó jegyzék iktatószáma</legend>
+  <input name="iktatoszam" type="text" value="">
+
+  <legend for="datum">Átadás/átvétel ideje</legend>
+  <input name="datum" type="date" value="">
+
+  <input type="subit" name="tovabb" vlaue="Mentés és tovább" title="Menti a jegyzék adatait és továbblép a cimkék rögzítéséhez" >
+  <input type="subit" name="mentes" vlaue="Mentés" >
+  <a href="/feltoltes" title="Mentés nélküli visszalépés" >Vissza</a>
+
+<?php
+ ?>
+
+</from>
