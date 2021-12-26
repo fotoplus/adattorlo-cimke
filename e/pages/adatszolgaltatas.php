d3e3a480257362ca2dff2d9ad3e955c907234ad2
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2021-12-27 00:25:25 +0100

    Update adatszolgaltatas.php

--- e/pages/adatszolgaltatas.php
+++ e/pages/adatszolgaltatas.php
@@ -1,3 +1,32 @@
-Ez még nem készült el.
+<?php 
 
-<a href="/" class="space">Vissza</a>
+$segments[1] = isset($segments[1]) ? $segments[1] : false;
+switch($segments[1]):
+  default:
+    ?>
+      <p>Ez még nem készült el.</p>
+      <a href="/" class="space">Vissza</a>
+      <a href="/adatszolgaltatas/teljes-lista" class="space">Teljes lekérdezés indítása</a>
+    <?php
+  break;
+  case "teljes-lista":
+    $query=('SELECT `cimke`.`sorszam` AS `cimke`, `kn` , `datum`, `telephely` FROM `cimke` LEFT JOIN `ertekesites` ON `ertekesites`.`sorszam`=`cimke`.`sorszam` WHERE 1');
+    $result = $mysqli->query($query);
+    echo '<table>';
+    while($row = $result->fetch_assoc()) {
+      echo <<<HTML
+        <tr>
+            <td>$row['cimke']</td>
+            <td>$row['datum']</td>
+            <td>substr($row['kn'], 0, 4)</td>
+            <td>$row['telephely']</td>
+        </tr>
+      HTML;
+
+    }
+    echo '</table>';
+    echo '<a href="/" class="space">Vissza</a>';
+  break;
+endswitch;
+
+?>
