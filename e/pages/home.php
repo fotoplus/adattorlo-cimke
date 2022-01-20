1c3c698e9f477ccab4f828d1af0935e601352923
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2022-01-20 11:41:38 +0100

    Update
    .cta osztály a főgomb kiemelésére.

--- e/pages/home.php
+++ e/pages/home.php
@@ -2,7 +2,8 @@
 
 <nav>
   <ul>
-    <li><a href="/ertekesites">Értékesítés</a></li>
+    <li><a href="/ertekesites" class="cta">Értékesítés</a></li>
+    <li><a href="/feltoltes">Címkék telephelyhez rendelése</a></li>
     <li><a href="/feltoltes">Új címkék felvitele</a></li>
     <li><a href="/adatszolgaltatas">Adatszolgáltatás</a></li>
   </ul>
