720367b898a0e4e7ef4433256f6191b1ae9984ce
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2022-01-20 11:31:20 +0100

    Update adatszolgaltatas.php

--- e/pages/adatszolgaltatas.php
+++ e/pages/adatszolgaltatas.php
@@ -4,10 +4,42 @@ $segments[1] = isset($segments[1]) ? $segments[1] : false;
 switch($segments[1]):
   default:
     ?>
-      <p>Ez még nem készült el.</p>
-      <a href="/" class="space">Vissza</a>
-      <a href="/adatszolgaltatas/teljes-lista" class="space">Teljes lekérdezés indítása</a>
+
+      <nav>  
+          <ul>    
+            <li><a href="/adatszolgaltatas/jegyzek-lista" class="space">Jegyzékek listázása</a> -- Fejlesztés alatt!</li>
+            <li><a href="/adatszolgaltatas/teljes-lekerdezes" class="space">Teljes lekérdezés indítása</a></li>
+          </ul>
+          <a href="/" class="space">Vissza</a>
+        </nav>
+
+      
+      
+      
     <?php
+  break;
+  case "jegyzek-lista":
+    $query=('SELECT `id`, `datum`, `iktatoszam` FROM `atadas-atvetel` ORDER BY `datum` ASC');
+    $result = $mysqli->query($query);
+    echo '<a href="/" class="space">Vissza</a>';
+    echo '<table style="border:1px solid #ccc; text-align: center;" width="100%">';
+    while($row = $result->fetch_assoc()) {
+      $vtsz= substr($row['kn'], 0, 4);
+      echo ('
+        <tr>
+            <td style="border:1px solid #ccc;">'.$row['iktatoszam'].'</td>
+            <td style="border:1px solid #ccc;">'.$row['datum'].'</td>
+            <td style="border:1px solid #ccc;"><button name="jegyzek" value="'.$row['id'].'" title="Lekérdezés indítása erre a jegyzékre">Lekérdezés</button></td>
+        </tr>');
+    }
+
+    echo '</table>';
+    echo '<a href="/" class="space">Vissza</a>';
+
+  break;
+  case "jegyzek-lekerdezes":
+    
+    echo '<a href="/" class="space">Vissza</a>';
   break;
   case "teljes-lista":
     $query=('SELECT `cimke`.`sorszam` AS `cimke`, `kn` , `datum`, `telephely` FROM `cimke` LEFT JOIN `ertekesites` ON `ertekesites`.`sorszam`=`cimke`.`sorszam` WHERE 1');
