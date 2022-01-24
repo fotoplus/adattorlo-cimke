86a67d3921c22a7d3501536aff29af86adc4b32e
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2022-01-24 16:01:38 +0100

    Update hozzarendeles.php

--- e/pages/hozzarendeles.php
+++ e/pages/hozzarendeles.php
@@ -3,10 +3,54 @@
   $segments[1] = isset($segments[1]) ? $segments[1] : false;
   switch($segments[1]):
     default:
+      if( isset($_POST['save']) and $_POST['save'] == 'hozzarendeles' ):
+
+        $telephely        = isset($_POST['telephely'])  ? $_POST['telephely']   : false;
+        $cimke['kezdet']  = isset($_POST['kezdet'])     ? $_POST['kezdet']      : false;
+        $cimke['veg']     = isset($_POST['veg'])        ? $_POST['veg']         : false;
+
+
+        if($telephely and $kezdet and $veg):
+          // Címke sorszám
+          $sorszam=$cimke['kezdet'];
+
+          // Számláló
+          $i=0;
+          
+          do {
+            $query = sprintf('SELECT * FROM `` WHERE `sorszam`="%s"', $mysqli->real_escape_string($sorszam));
+            $result = $mysqli->query($query);
+            $count = $result->num_rows;
+        
+            if($count != 1):
+              $err='<p>A(z) <span class="bold">'.$sorszam.'</span> sorszámú címke nem létezik.</p>';
+            else:
+              $stmt = $mysqli->prepare('INSERT INTO `cimke` (`tid`) VALUES (?)');
+              $stmt->bind_param('s', $telephely);
+
+              if ( $stmt->execute() ) :
+                $log .= 'A <span class="green">' . $sorszam . '</span> sorszámú címke hozzáadva.'.chr(13);
+              else:
+                // Ez mondjuk hiba esetén nem jelenik meg, mert már feljebb megáll.
+                $log .= '<span class="red">Hiba (' . $sorszam . '): '.$stmt->errorCode().'</span>'.chr(13);
+              endif;
+            endif;
+
+            $sorszam++; 
+            $i++; // Számláló
+
+          } while($sorszam <= $cimke['veg']);
+
+          $msg = '<p>' . $i . ' új címke került a rendszerbe.</p>';
+          $msg = '<p>Ha hibát írt ki, vissza kell vonni a műveletet.<br>Szükség esetén készíts képernyőképet és kérj segítséget!</p>';
+          $cimke = false;
+        else:
+          $msg='<p>Hiányzó adatok.</p>';
+          die($msg);
+        endif;
+
+      endif;
       ?>
-      <nav>  
-           <a href="/" class="space">Vissza</a>
-      </nav>
       <form method="post" action="/hozzarendeles/" autocomplete="off">
           <fieldset>
               <legend>Címke intervallum telephelyhez rendelése</legend>
