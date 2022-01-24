fa94555faa215e531d1a2ba6bb23a88af55b4be7
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2022-01-24 10:35:31 +0100

    Update hozzarendeles.php

--- e/pages/hozzarendeles.php
+++ e/pages/hozzarendeles.php
@@ -4,15 +4,45 @@
   switch($segments[1]):
     default:
       ?>
-        <nav>  
-          <ul>    
-            <li><a href="/hozzarendeles/" style="">Új átadás-átvételi jegyzék</a></li>
+      <nav>  
+           <a href="/" class="space">Vissza</a>
+      </nav>
+      <form method="post" action="/hozzarendeles/" autocomplete="off">
+          <fieldset>
+              <legend>Címke intervallum telephelyhez rendelése</legend>
+              <?php
+              if($log):
+                print '<pre id="log" class="rounded-main">' . $log . '</pre>';
+              endif;
 
-          </ul>
-          <a href="/" class="space">Vissza</a>
-          
-        </nav>
+              if($msg):
+                print'<div class="msg rounded-main border-main">';
+                print $msg;
+                print '</div>';
+              endif;
+              ?>
 
+              <label>Telephely</label>
+              <div class="radio-group">
+                <?php
+                  $query = ('SELECT * FROM `telephelyek` ORDER BY `id` ASC');
+                  $result = $mysqli->query($query);
+                  while($telephely = $result->fetch_assoc()) {
+                      echo('<input id="telephely' . $telephely['id'] . '" type="radio" name="telephely" value="' . $telephely['id'] .'" label="'.$telephely['name'].'" title="'.$telephely['name'].'" required>');
+                  }
+                ?>
+              </div>
+
+              <label for="kezdet">Sorszámok kezdete</label>
+              <input name="kezdet" type="number" value="<?php print $cimke['kezdet']; ?>" required>
+
+              <label for="veg">Sorszámok vége</label>
+              <input name="veg" type="number" value="<?php print $cimke['veg']; ?>" required>
+
+              <a href="/" title="Mentés nélküli visszalépés" class="space" >Vissza</a>
+          </fieldset>
+        </form>
+      </form>
       <?php
     break;
   endswitch;
