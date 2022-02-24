e39ad7fa550d82e3f654ac0bf15b9697c33e5a1a
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2022-02-24 16:14:55 +0100

    Update ipcheck.php

--- e/modules/accesscontrol/ipcheck.php
+++ e/modules/accesscontrol/ipcheck.php
@@ -7,6 +7,8 @@ if(isset($_SERVER['REMOTE_ADDR']) and !empty($_SERVER['REMOTE_ADDR'])):
     $query = sprintf('SELECT `telephelyek`.`id` AS `branch_id`, `telephelyek`.`name` AS `branch_name` FROM `ip_cimek` LEFT JOIN `telephelyek` ON `ip_cimek`.`tid` = `telephelyek`.`id` WHERE `ip_cimek`.`ipv4`="%s"', $mysqli->real_escape_string($ip));
     $result = $mysqli->query($query);
     $count = $result->num_rows;
+    $result = $result->fetch_assoc();
+
 
     if($count == 1):
         
