a7642503b6b6ed9d849daacbd16f950142be0f00
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2022-02-25 11:37:12 +0100

    Update adatszolgaltatas.php

--- e/pages/adatszolgaltatas.php
+++ e/pages/adatszolgaltatas.php
@@ -26,7 +26,15 @@ switch($segments[1]):
         <a href="/adatszolgaltatas/jegyzek-lekerdezes" class="space">Vissza</a>
       HTML;
 
-      $query=('SELECT `cimke`.`sorszam` AS `cimke`, `kn` , `datum`, `tszam`, `doboz`, `csomag` , `ertekesites`.`tid` AS `tid`, `cimke`.`tid` AS `c_tid`,  `telephelyek`.`name` AS `telephely` FROM `cimke` LEFT JOIN `ertekesites` ON `ertekesites`.`sorszam`=`cimke`.`sorszam` LEFT JOIN `telephelyek` ON `ertekesites`.`tid` = `telephelyek`.`id`,  `cimke`.`tid` = `telephelyek`.`id` WHERE `cimke`.`aid` = %s');
+      $query = ('SELECT * FROM `telephelyek` ORDER BY `id` ASC');
+      $result = $mysqli->query($query);
+      $telephely = $result->fetch_array(MYSQLI_ASSOC);
+
+      print_r($telephely);
+      die;
+
+
+      $query=('SELECT `cimke`.`sorszam` AS `cimke`, `kn` , `datum`, `tszam`, `doboz`, `csomag` , `ertekesites`.`tid` AS `tid`, `cimke`.`tid` AS `c_tid`,  `telephelyek`.`name` AS `telephely` FROM `cimke` LEFT JOIN `ertekesites` ON `ertekesites`.`sorszam`=`cimke`.`sorszam` WHERE `cimke`.`aid` = %s');
       $query = sprintf($query, $mysqli->real_escape_string($_POST['jegyzek']));
       $result = $mysqli->query($query);
       echo <<<HTML
